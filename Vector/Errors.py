21a17a4fe169692a569db67549311431389fa5d3
Fedor Kuznetsov <kuzneczov-f@susanoo.ru>
2022-09-30 14:55:58 +0800

    Рефакторинг Vector.py
    if type(vector_arr1) == int or type(vector_arr1) == float
    заменено на функцию is_scalar (проверяет аргументов является скаляром или нет)
    
    убраны повторяющиеся элементы в функциях mul_vectors, div_vectors
    вынесены в функцию sort_arguments (сортирует и возвращает (массив вектора, скаляр))
    
    новая функция в Errors.py - ensure_types_vectors: проверяет два вектора на типы

--- Vector/Errors.py
+++ Vector/Errors.py
@@ -4,10 +4,17 @@ def ensure_shape_vectors(arr1, arr2):
     if len(arr1) != len(arr2):
         raise ValueError("Error of different lengths vectors")
 
+
 def ensure_types_vector(arr):
     for point in arr:
         if type(point) != int and type(point) != float:
             raise TypeError("An incorrect type in vector")
+
+
+def ensure_types_vectors(arr1, arr2):
+    ensure_types_vector(arr1)
+    ensure_types_vector(arr2)
+
 
 def ensure_full_vector(arr1, arr2):
     ensure_shape_vectors(arr1, arr2)
@@ -38,6 +45,7 @@ def ensure_types_matrix(arr):
 def ensure_shape_matrix_mul(arr1, arr2):
     if len(arr1[0]) != len(arr2):
         raise ValueError("Cannot multiply the two matrices, incorrect dimensions")
+
 
 def ensure_full_matrix(arr1, arr2):
     pass
--- Vector/Vector.py
+++ Vector/Vector.py
@@ -1,5 +1,5 @@
 import math
-from Errors import *
+from .Errors import *
 
 
 def after_comma(number, digit=0):
@@ -57,48 +57,50 @@ def dif_vectors(vector_arr1, vector_arr2):
     return [vector_arr1[i] - vector_arr2[i] for i in range(len(vector_arr1))]
 
 
+def is_scalar(vector_arr):
+    return type(vector_arr) == int or type(vector_arr) == float
+
+
+def sort_arguments(vector_arr1, vector_arr2):
+    if is_scalar(vector_arr1):
+        ensure_types_vector(vector_arr2)
+        return vector_arr2, vector_arr1
+    return vector_arr1, vector_arr2
+
+
 def mul_vectors(vector_arr1, vector_arr2):
     ''' Return multiplication of vectors or multiplying a vector by a scalar '''
-    if type(vector_arr1) == int or type(vector_arr1) == float:
-        ensure_types_vector(vector_arr2)
-        return [vector_arr2[i] * vector_arr1 for i in range(len(vector_arr2))]
-    elif type(vector_arr2) == int or type(vector_arr2) == float:
-        ensure_types_vector(vector_arr1)
-        return [vector_arr1[i] * vector_arr2 for i in range(len(vector_arr1))]
+    if is_scalar(vector_arr1) or is_scalar(vector_arr2):
+        vector_arr1, scal = sort_arguments(vector_arr1, vector_arr2)
+        return [vector_arr1[i] * scal for i in range(len(vector_arr1))]
     ensure_full_vector(vector_arr1, vector_arr2)
     return [vector_arr1[i] * vector_arr2[i] for i in range(len(vector_arr1))]
 
 
 def div_vectors(vector_arr1, vector_arr2):
     ''' Return division of vectors or dividing a vector by a scalar '''
-    if type(vector_arr1) == int or type(vector_arr1) == float:
-        ensure_types_vector(vector_arr2)
-        return [vector_arr2[i] / vector_arr1 for i in range(len(vector_arr2))]
-    elif type(vector_arr2) == int or type(vector_arr2) == float:
-        ensure_types_vector(vector_arr1)
-        return [vector_arr1[i] / vector_arr2 for i in range(len(vector_arr1))]
+    if is_scalar(vector_arr1) or is_scalar(vector_arr2):
+        vector_arr1, scal = sort_arguments(vector_arr1, vector_arr2)
+        return [vector_arr1[i] / scal for i in range(len(vector_arr1))]
     ensure_full_vector(vector_arr1, vector_arr2)
     return [vector_arr1[i] / vector_arr2[i] for i in range(len(vector_arr1))]
 
 
 def is_collinear(vector_arr1, vector_arr2, digit=5):
     ''' Checks vectors for collinearity with the specified accuracy (default is 5) '''
-    ensure_types_vector(vector_arr1)
-    ensure_types_vector(vector_arr2)
+    ensure_types_vectors(vector_arr1, vector_arr2)
     return abs(cos_vectors(vector_arr1, vector_arr2, digit)) == 1.0
 
 
 def is_directional(vector_arr1, vector_arr2, digit=5):
     ''' Checks vectors for co-directionality with a given accuracy (default is 5) '''
-    ensure_types_vector(vector_arr1)
-    ensure_types_vector(vector_arr2)
+    ensure_types_vectors(vector_arr1, vector_arr2)
     return cos_vectors(vector_arr1, vector_arr2, digit) == 1.0
 
 
 def is_not_directional(vector_arr1, vector_arr2, digit=3):
     ''' Returns the orthogonality value of vectors with a given accuracy (default is 5) '''
-    ensure_types_vector(vector_arr1)
-    ensure_types_vector(vector_arr2)
+    ensure_types_vectors(vector_arr1, vector_arr2)
     return cos_vectors(vector_arr1, vector_arr2, digit) == -1.0
 
 
@@ -110,15 +112,13 @@ def change_direction(vector_arr):
 
 def projection(vector_arr1, vector_arr2):
     ''' Returns the projection of a vector onto a vector '''
-    ensure_types_vector(vector_arr1)
-    ensure_types_vector(vector_arr2)
+    ensure_types_vectors(vector_arr1, vector_arr2)
     return scalar(vector_arr1, vector_arr2) / lenght(vector_arr2)
 
 
 def is_orthogonal(vector_arr1, vector_arr2, digit=5):
     ''' Returns the orthogonality value of vectors with a given accuracy (default is 5) '''
-    ensure_types_vector(vector_arr1)
-    ensure_types_vector(vector_arr2)
+    ensure_types_vectors(vector_arr1, vector_arr2)
     return cos_vectors(vector_arr1, vector_arr2, digit) == 0
 
 
