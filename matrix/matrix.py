7ae99809a67b8b2190c88332d38fbd4369c0128b
Fedor Kuznetsov <kuzneczov-f@susanoo.ru>
2022-10-12 21:58:19 +0800

    тройные апострофы -> тройные двойные кавычки

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -3,19 +3,19 @@ from errors import *
 
 
 def sum_matrix(matrix1, matrix2):
-    ''' Return sum of matrices '''
+    """ Return sum of matrices """
     ensure_full_matrix(matrix1, matrix2)
     return [vec.sum_vectors(matrix1[IX_cell], matrix2[IX_cell]) for IX_cell in range(len(matrix1))]
 
 
 def dif_matrix(matrix1, matrix2):
-    ''' Return difference of matrices '''
+    """ Return difference of matrices """
     ensure_full_matrix(matrix1, matrix2)
     return [vec.dif_vectors(matrix1[IX_cell], matrix2[IX_cell]) for IX_cell in range(len(matrix1))]
 
 
 def transportation_matrix(matrix):
-    ''' Transposes the matrix '''
+    """ Transposes the matrix """
     ensure_types_matrix(matrix)
     return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
 
@@ -29,7 +29,7 @@ def sort_arguments(matrix1, matrix2):
 
 
 def mul_matrix(matrix1, matrix2):
-    ''' Return multiplication of matrices or multiplying a matrix by a scalar '''
+    """ Return multiplication of matrices or multiplying a matrix by a scalar """
     if vec.is_scalar(matrix1) or vec.is_scalar(matrix2):
         matrix1, scal = sort_arguments(matrix1, matrix2)
         return [vec.mul_vectors(matrix1[IX_row], scal) for IX_row in range(len(matrix1))]
@@ -48,36 +48,38 @@ def mul_matrix(matrix1, matrix2):
 
 
 def get_row(matrix, row):
-    ''' Returns a row from the matrix by index '''
+    """ Returns a row from the matrix by index """
     return matrix[row]
 
 
 def get_column(matrix, column):
-    ''' Returns a column from the matrix by index '''
+    """ Returns a column from the matrix by index """
     return get_row(transportation_matrix(matrix), column)
 
 
 def row_switch(matrix, row1, row2):
-    ''' Returns a matrix with permuted rows by indexes '''
+    """ Returns a matrix with permuted rows by indexes """
     matrix[row1 - 1], matrix[row2 - 1] = matrix[row2 - 1], matrix[row1 - 1]
     return matrix
 
 
 def mul_matrix_row(matrix, row, scalar):
-    ''' Multiplies a row from the matrix by a given index by a scalar '''
+    """ Multiplies a row from the matrix by a given index by a scalar """
     ensure_types_matrix(matrix)
     matrix[row - 1] = vec.mul_vectors(matrix[row - 1], scalar)
     return matrix
 
+
 def sum_matrix_rows(matrix, row1, row2, scalar):
-    ''' Returns the matrix in which the rows are stacked, which are multiplied by a scalar '''
+    """ Returns the matrix in which the rows are stacked, which are multiplied by a scalar """
     ensure_types_matrix(matrix)
     matrix = [row for row in matrix]
     matrix[row1 - 1] = vec.sum_vectors(matrix[row1 - 1], vec.mul_vectors(get_row(matrix, row2), scalar))
     return matrix
 
+
 def dif_matrix_rows(matrix, row1, row2, scalar):
-    ''' Returns a matrix in which the rows are subtracted, which are multiplied by a scalar '''
+    """ Returns a matrix in which the rows are subtracted, which are multiplied by a scalar """
     ensure_types_matrix(matrix)
     matrix = [row for row in matrix]
     matrix[row1 - 1] = vec.dif_vectors(matrix[row1 - 1], vec.mul_vectors(get_row(matrix, row2), scalar))
--- vector/vector.py
+++ vector/vector.py
@@ -3,12 +3,12 @@ from errors import *
 
 
 def after_comma(number, digit=0):
-    ''' Number of decimal places '''
+    """ Number of decimal places """
     return round(number, digit)
 
 
 def lenght(vector_arr):
-    ''' Returns the length of the vector '''
+    """ Returns the length of the vector """
     ensure_types_vector(vector_arr)
     trunk = 0
     for coord in vector_arr:
@@ -17,7 +17,7 @@ def lenght(vector_arr):
 
 
 def scalar(vector_arr1, vector_arr2):
-    ''' Returns the scalar product of vectors '''
+    """ Returns the scalar product of vectors """
     ensure_full_vector(vector_arr1, vector_arr2)
     trunk = 0
     for i in range(len(vector_arr1)):
@@ -26,32 +26,32 @@ def scalar(vector_arr1, vector_arr2):
 
 
 def normalize(vector_arr):
-    ''' Returns a normalized vector '''
+    """ Returns a normalized vector """
     ensure_types_vector(vector_arr)
     return [i / lenght(vector_arr) for i in vector_arr]
 
 
 def cos_vectors(vector_arr1, vector_arr2, digit=5):
-    ''' Return the cos value between two vectors with the specified accuracy (default is 5) '''
+    """ Return the cos value between two vectors with the specified accuracy (default is 5) """
     ensure_full_vector(vector_arr1, vector_arr2)
     return after_comma(scalar(vector_arr1, vector_arr2) / (lenght(vector_arr1) * lenght(vector_arr2)), digit)
 
 
 def angular(vector_arr1, vector_arr2, digit=5):
-    ''' Returns the angle between vectors with the specified accuracy (default is 5) '''
+    """ Returns the angle between vectors with the specified accuracy (default is 5) """
     cos = cos_vectors(vector_arr1, vector_arr2, digit)
     rad = math.acos(cos)
     return round(rad / math.pi * 180, digit)
 
 
 def sum_vectors(vector_arr1, vector_arr2):
-    ''' Return sum of vectors '''
+    """ Return sum of vectors """
     ensure_full_vector(vector_arr1, vector_arr2)
     return [vector_arr1[i] + vector_arr2[i] for i in range(len(vector_arr1))]
 
 
 def dif_vectors(vector_arr1, vector_arr2):
-    ''' Return difference of vectors '''
+    """ Return difference of vectors """
     ensure_full_vector(vector_arr1, vector_arr2)
     return [vector_arr1[i] - vector_arr2[i] for i in range(len(vector_arr1))]
 
@@ -69,7 +69,7 @@ def sort_arguments(vector_arr1, vector_arr2):
 
 
 def mul_vectors(vector_arr1, vector_arr2):
-    ''' Return multiplication of vectors or multiplying a vector by a scalar '''
+    """ Return multiplication of vectors or multiplying a vector by a scalar """
     if is_scalar(vector_arr1) or is_scalar(vector_arr2):
         vector_arr1, scal = sort_arguments(vector_arr1, vector_arr2)
         return [vector_arr1[i] * scal for i in range(len(vector_arr1))]
@@ -78,7 +78,7 @@ def mul_vectors(vector_arr1, vector_arr2):
 
 
 def div_vectors(vector_arr1, vector_arr2):
-    ''' Return division of vectors or dividing a vector by a scalar '''
+    """ Return division of vectors or dividing a vector by a scalar """
     if is_scalar(vector_arr1) or is_scalar(vector_arr2):
         vector_arr1, scal = sort_arguments(vector_arr1, vector_arr2)
         return [vector_arr1[i] / scal for i in range(len(vector_arr1))]
@@ -87,37 +87,37 @@ def div_vectors(vector_arr1, vector_arr2):
 
 
 def is_collinear(vector_arr1, vector_arr2, digit=5):
-    ''' Checks vectors for collinearity with the specified accuracy (default is 5) '''
+    """ Checks vectors for collinearity with the specified accuracy (default is 5) """
     ensure_types_vectors(vector_arr1, vector_arr2)
     return abs(cos_vectors(vector_arr1, vector_arr2, digit)) == 1.0
 
 
 def is_directional(vector_arr1, vector_arr2, digit=5):
-    ''' Checks vectors for co-directionality with a given accuracy (default is 5) '''
+    """ Checks vectors for co-directionality with a given accuracy (default is 5) """
     ensure_types_vectors(vector_arr1, vector_arr2)
     return cos_vectors(vector_arr1, vector_arr2, digit) == 1.0
 
 
 def is_not_directional(vector_arr1, vector_arr2, digit=3):
-    ''' Returns the orthogonality value of vectors with a given accuracy (default is 5) '''
+    """ Returns the orthogonality value of vectors with a given accuracy (default is 5) """
     ensure_types_vectors(vector_arr1, vector_arr2)
     return cos_vectors(vector_arr1, vector_arr2, digit) == -1.0
 
 
 def change_direction(vector_arr):
-    ''' Changes the direction of the vector '''
+    """ Changes the direction of the vector """
     ensure_types_vector(vector_arr)
     return [vector_arr[i] * -1 for i in range(len(vector_arr))]
 
 
 def projection(vector_arr1, vector_arr2):
-    ''' Returns the projection of a vector onto a vector '''
+    """ Returns the projection of a vector onto a vector """
     ensure_types_vectors(vector_arr1, vector_arr2)
     return scalar(vector_arr1, vector_arr2) / lenght(vector_arr2)
 
 
 def is_orthogonal(vector_arr1, vector_arr2, digit=5):
-    ''' Returns the orthogonality value of vectors with a given accuracy (default is 5) '''
+    """ Returns the orthogonality value of vectors with a given accuracy (default is 5) """
     ensure_types_vectors(vector_arr1, vector_arr2)
     return cos_vectors(vector_arr1, vector_arr2, digit) == 0
 
